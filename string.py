286eb6bda0cca957f094f4a16554b3172cdfd7bf
mr-kaveh <mr.hdavoodi@gmail.com>
2018-05-18 00:11:49 +0430

    string chapter

--- string.py
+++ string.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 
 if __name__ == '__main__':
+	#some string builtins
 	res = subprocess.Popen(['uname', '-sv'], stdout=subprocess.PIPE)
 	uname = res.stdout.read().strip()
 	print(uname)
@@ -13,8 +14,11 @@ if __name__ == '__main__':
 	print(type(uname))
 	print(uname[1:])
 	print(' '.join(uname.split(' ')[1:]))
+	#lists Operations
 	some_list = range(10)
+	#join generally can be used to convert lists to strings
 	print(','.join([str(i) for i in some_list]))
+	#some directory manipulation
 	directory = subprocess.Popen(['pwd'], stdout=subprocess.PIPE)
 	folder = directory.stdout.read().strip().split('/')
 	print(''.join(folder[len(folder)-1:]))
