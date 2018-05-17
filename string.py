6c42fcc5278cb440d865d08ddef5ff43bfb51331
mr-kaveh <mr.hdavoodi@gmail.com>
2018-05-18 00:07:38 +0430

    string chapter

--- string.py
+++ string.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import subprocess
+import os
 
 if __name__ == '__main__':
 	res = subprocess.Popen(['uname', '-sv'], stdout=subprocess.PIPE)
@@ -7,4 +8,17 @@ if __name__ == '__main__':
 	print(uname)
 	print('Linux' in uname)
 	print('Darwin' not in uname)
-
+	print(uname.index('Linux'))
+	print(uname.find('Unix'))
+	print(type(uname))
+	print(uname[1:])
+	print(' '.join(uname.split(' ')[1:]))
+	some_list = range(10)
+	print(','.join([str(i) for i in some_list]))
+	directory = subprocess.Popen(['pwd'], stdout=subprocess.PIPE)
+	folder = directory.stdout.read().strip().split('/')
+	print(''.join(folder[len(folder)-1:]))
+	textFilesProc = subprocess.Popen(['find', '/', '-name', '*.txt'], stdout=subprocess.PIPE)
+	textFiles = textFilesProc.stdout.read().strip().split('\n')
+	print(len(textFiles))
+	subprocess.call(['cat', textFiles[500]], shell=False)
