91b6a2807c19cac0d660d73a045760e2db9947d1
mr-kaveh <mr.hdavoodi@gmail.com>
2019-02-11 12:25:15 +0330

    adding sys.ArgV

--- sysModule/sysArgv.py
+++ sysModule/sysArgv.py
@@ -7,6 +7,9 @@ if __name__ == '__main__':
     sys.argv is a list which contains 
     the command-line Arguments passed
     to the Python Script
+    
+    if you are intending to work with command-line 
+    Arguments, You Probably want to utilize sys.argv
     '''
     print('this is the Script Name: ', sys.argv[0])
     print('Argv Length :', len(sys.argv))
