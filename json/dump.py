c2b79c9dc0f00fb12b9b783c59118b210ac2e5e2
mr-kaveh <mr.hdavoodi@gmail.com>
2019-02-26 10:12:53 +0330

    JSON Library

---
+++ json/dump.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3.6
+
+import json
+import requests
+
+data = {
+    "president": {
+        "name": "Ghazanfar Darko",
+        "species": "GoldenMiner"
+    }
+}
+json_string = """
+{
+    "researcher": {
+        "name": "Ford Prefect",
+        "species": "Betelgeusian",
+        "relatives": [
+            {
+                "name": "Zaphod Beeblebrox",
+                "species": "Betelgeusian"
+            }
+        ]
+    }
+}
+"""
+
+print(json.dumps(data, indent=4)) # Serialization
+
+with open('output_file.json', 'w') as write_file:
+    json.dump(data, write_file) # Serialization
+
+
+data = json.loads(json_string) # Deserialization
+print(data)
+
+response = requests.get("https://jsonplaceholder.typicode.com/todos")
+todos = json.loads(response.text) # Deserialization
+print(todos)
