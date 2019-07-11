ee9ba5fcdb2f8473e78b2cf6300066c590aa0ea5
mr-kaveh <mr.hdavoodi@gmail.com>
2019-07-11 14:52:36 +0430

    updating some of the functions

--- sysctl_conf.py
+++ sysctl_conf.py
@@ -56,9 +56,54 @@ def read_parameters(pattern):
         print(ex)
 
 
-def write(tunable):
+def write_security():
+    tunables = ['fs.suid_dumpable=0',
+                'kernel.randomize_va_space=2',
+                'net.ipv4.ip_forward=0',
+                'net.ipv4.conf.all.send_redirects=0',
+                'net.ipv4.conf.default.send_redirects=0',
+                'net.ipv4.conf.all.accept_source_route=0',
+                'net.ipv4.conf.default.accept_source_route=0',
+                'net.ipv4.conf.all.accept_redirects=0',
+                'net.ipv4.conf.default.accept_redirects=0',
+                'net.ipv4.conf.all.secure_redirects=0',
+                'net.ipv4.conf.default.secure_redirects=0',
+                'net.ipv4.conf.all.log_martians=1',
+                'net.ipv4.conf.default.log_martians=1',
+                'net.ipv4.icmp_echo_ignore_broadcasts=1',
+                'net.ipv4.icmp_ignore_bogus_error_responses=1',
+                'net.ipv4.conf.all.rp_filter=1',
+                'net.ipv4.conf.default.rp_filter=1',
+                'net.ipv4.tcp_syncookies=1',
+                'net.ipv6.conf.all.accept_ra=0',
+                'net.ipv6.conf.default.accept_ra=0',
+                'net.ipv6.conf.all.accept_redirects=0',
+                'net.ipv6.conf.default.accept_redirects=0']
     try:
-        os.system(f'echo {tunable} >> /etc/sysctl.conf')
+        with open('/etc/sysctl.d/90-security.conf', 'w+') as conf_file:
+            for i in tunables:
+                conf_file.write(i + '\n')
+        os.system('sysctl -w net.ipv4.route.flush=1 && sysctl -p /etc/sysctl.d/90-security.conf')
+    except OSError as ex:
+        print(ex)
+
+
+def write(tunable: str):
+    existing_value = ''
+    try:
+        with open('/etc/sysctl.conf', 'r') as sysctl_file:
+            for i in sysctl_file:
+                if str(i.split('=')[0]) == tunable.split('=')[0]:
+                    existing_value = str(i.strip('\n'))
+                    break
+                else:
+                    continue
+        if existing_value != '':
+            os.system(f'sudo sed -i "s/{existing_value}/{tunable}/g" /etc/sysctl.conf && sysctl -p')
+            print(f'{existing_value} is replaced by {tunable}')
+        else:
+            os.system(f'echo {tunable} >> /etc/sysctl.conf && sysctl -p')
+            print(f'value {tunable} has been added to sysctl.conf')
     except OSError as ex:
         print(ex)
 
@@ -77,4 +122,12 @@ if __name__ == '__main__':
         """
         read_parameters(args.pattern)
     elif args.input == 'write':
-        write('vm.swappiness=40')
+        """
+            Run as follows:
+                #./sysctl_conf.py write tunable
+        """
+        # outPut = subprocess.Popen(['sysctl', '-p'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # if len(bytes.decode(outPut.communicate()[0], 'utf-8').split('\n')) > 0:
+        write(args.tunable)
+    elif args.input == 'security':
+        write_security()
